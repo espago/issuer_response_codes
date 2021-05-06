312006bbd9e9f89d66650d91e8065ba707b6059b
Mateusz Drewniak <m.drewniak@espago.com>
2021-05-07 00:15:07 +0200

    README properly filled

--- test/issuer_response_codes/code_test.rb
+++ test/issuer_response_codes/code_test.rb
@@ -25,7 +25,7 @@ module IssuerResponseCodes
       assert_equal :merchant, code.target
       assert_equal :en, code.locale
       assert code.fraud_notice
-      assert_equal 'Stolen card. Please contact your card issuer to get more details and try again later. IMPORTANT NOTICE: It is forbidden to retry transactions that ended with this code. It may be recognized as fraud attempt!', code.humanize
+      assert_equal 'Stolen card. Please contact your card issuer to get more details and try again later. IMPORTANT NOTICE: It is forbidden to retry transactions that ended with this code. It may be recognized as a fraud attempt!', code.humanize
       
       code = IssuerResponseCodes::Code.new(id: "43", target: :cardholder)
       assert_equal :cardholder, code.target
@@ -40,6 +40,46 @@ module IssuerResponseCodes
       assert_equal :en, code.locale
       assert code.fraud_notice
       assert_equal 'Unknown reason. Please contact our support team.', code.humanize
+    end
+
+    def test_description_return_proper_value
+      code = IssuerResponseCodes::Code.new(id: "00")
+      assert_equal :merchant, code.target
+      assert_equal :en, code.locale
+      assert code.fraud_notice
+      assert_equal "Transaction rejected due to no response from issuer/bank, inactive Merchant account, used not supported card or incorrect card data. Check the card's data or please try again later.", code.description
+      
+      code = IssuerResponseCodes::Code.new(id: "12")
+      assert_equal :merchant, code.target
+      assert_equal :en, code.locale
+      assert code.fraud_notice
+      assert_equal 'No privileges to execute this transaction for your card. Please contact your card issuer to get more details and try again later.', code.description
+      
+      code = IssuerResponseCodes::Code.new(id: "54")
+      assert_equal :merchant, code.target
+      assert_equal :en, code.locale
+      assert code.fraud_notice
+      assert_equal 'Expired card. Please check your card or try another.', code.description
+
+      code = IssuerResponseCodes::Code.new(id: "43")
+      assert_equal :merchant, code.target
+      assert_equal :en, code.locale
+      assert code.fraud_notice
+      assert_equal 'Stolen card. Please contact your card issuer to get more details and try again later. IMPORTANT NOTICE: It is forbidden to retry transactions that ended with this code. It may be recognized as a fraud attempt!', code.description
+      
+      code = IssuerResponseCodes::Code.new(id: "43", target: :cardholder)
+      assert_equal :cardholder, code.target
+      assert_equal :en, code.locale
+      assert !code.fraud_notice
+      assert_equal 'Your bank has declined this transaction. Please contact your card issuer to get more details and try again later. ', code.description
+    end
+
+    def test_description_return_unknown_notice
+      code = IssuerResponseCodes::Code.new(id: "XX")
+      assert_equal :merchant, code.target
+      assert_equal :en, code.locale
+      assert code.fraud_notice
+      assert_equal 'Unknown reason. Please contact our support team.', code.description
     end
 
     def test_behaviour_return_proper_value
@@ -77,7 +117,7 @@ module IssuerResponseCodes
       assert_equal :merchant, code.target
       assert_equal :en, code.locale
       assert code.fraud_notice
-      assert_equal 'Please contact your card issuer to get more details and try again later. IMPORTANT NOTICE: It is forbidden to retry transactions that ended with this code. It may be recognized as fraud attempt!', code.behaviour
+      assert_equal 'Please contact your card issuer to get more details and try again later. IMPORTANT NOTICE: It is forbidden to retry transactions that ended with this code. It may be recognized as a fraud attempt!', code.behaviour
     end
 
     def test_behaviour_return_unknown_notice
